0e02399571d8c188899fd8588070f83f8aa71e4f
Paulo Sergio Volmar Cezario Neto <p.sergiovolmar10@gmail.com>
2025-07-27 20:14:59 -0300

    1/3

--- jogo.py
+++ jogo.py
@@ -1,23 +1,24 @@
 from random import randint
 print('******JOGO DO NÚMERO SECRETO******')
-print('\n')
+
 nome = input('\nOlá! Tudo bem? Digite seu nome ao lado ^^ -> ')
+print('\n')
 print(f'Seja muito bem vindo(a) {nome.title()}! Vamos jogar um jogo?')
-print('Bom... O jogo consiste em você tentar adivinhar um número secreto no intervalo de 1 à 10.')
+print('Bom... O jogo consiste em você tentar adivinhar um número secreto no intervalo de 1 à 20.')
 print('Vamos lá!')
-x = randint(1,10)
+x = randint(1,20)
 print('\n')
 print('Gerando nosso número secreto...')
 print('\n')
-for tentativa in range(1,6):
+for tentativa in range(1,4):
   try:
-    alternativa = int(input(f'\nPalpite {tentativa}/5 ao número secreto: '))
+    alternativa = int(input(f'\nPalpite {tentativa}/3 ao número secreto: '))
     print('\n')
   except ValueError:
     print('\nVocê não digitou um número, tente novamente.')
   else:
-    if alternativa<=0 or alternativa>=10:
-      print(f'{alternativa} está fora do intervalo de 1 à 10.')
+    if alternativa<=0 or alternativa>=20:
+      print(f'{alternativa} está fora do intervalo de 1 à 20.')
     if alternativa==x:
       print(f'Parabéns {nome.title()}! Você acertou e o número secreto é {alternativa} ^^')
       break
